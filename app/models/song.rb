a80a15ef21bd920f146fd809726af1a848f308e6
Tyler Sell <tylerselldesign@gmail.com>
2019-03-03 22:21:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,7 @@ class Song < ActiveRecord::Base
   belongs_to :artists
   has_many :song_genres
   has_many :genres, :through => :song_genres
+  
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods 
 end
